c11d0cf62824599aa79088452cec2c0870d32aa2
viktorChubyrka <viktor2000897@gmail.com>
2021-08-10 13:43:31 +0300

    Responsive layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,36 @@
 <template>
     <div>
-        <b-navbar type="dark" variant="dark">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
             <b-navbar-brand href="#"><img class="header-logo" src="/images/logo-header.png" alt=""></b-navbar-brand>
-            <b-collapse is-nav>
-                <b-navbar-nav class="text-white">
-                    <b-nav-item-dropdown  text="Tasks">
+            <b-navbar-toggle class="m-1" target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="text-white col">
+                    <b-nav-item-dropdown 
+                        :class="`m-2 ${$route.path.split('/')[1] === 'tasks' ? 'show' : ''}`" 
+                        :text="`Tasks(${ $route.name })`"
+                    >
                         <b-dropdown-item to="/tasks/active">Active</b-dropdown-item>
                         <b-dropdown-item to="/tasks/complited">Complited</b-dropdown-item>  
                     </b-nav-item-dropdown>
-                    <b-nav-item>Feature 1</b-nav-item>
-                    <b-nav-item>Feature 2</b-nav-item>
-                    <b-nav-item>Feature 3</b-nav-item>
-                    <b-nav-item>Feature 4</b-nav-item>
+                    <b-nav-item class="m-2">Feature 1</b-nav-item>
+                    <b-nav-item class="m-2">Feature 2</b-nav-item>
+                    <b-nav-item class="m-2">Feature 3</b-nav-item>
+                    <b-nav-item class="m-2">Feature 4</b-nav-item>
                 </b-navbar-nav>
-            </b-collapse>
-            <b-collapse is-nav class="ml-auto justify-content-end">
-                <b-form-input class="w-50 m-2" placeholder="Search"></b-form-input>
-                <b-button class="m-2"  variant="outline-success">Search</b-button>
-            </b-collapse>
+                <b-row class="w-30">
+                    <b-col cols="7">
+                        <b-form-input placeholder="Search"></b-form-input>
+                    </b-col>
+                    <b-col cols="3">
+                        <b-button  variant="outline-success">Search</b-button>
+                    </b-col>
+                </b-row>
+            </b-collapse>  
         </b-navbar>
     </div>
 </template>
+<style scoped>
+.ds{
+    justify-content: space-between;
+}
+</style>
